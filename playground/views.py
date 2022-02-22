daf1b6e03e4346b6818c5a38e2d384ee479f9802
gsteeds <steeds.g@gmail.com>
2022-02-22 00:10:34 -0600

    Removed scraper from project, as well as cleaned up the playground app. Added .gitignore, and README.MD.

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,3 @@
 from django.http import HttpRequest
 from django.db.models.aggregates import Count, Avg
 from django.shortcuts import render
-
-from items.models import Weapon
-
-
-def say_hello(request):
-    count = Weapon.objects.aggregate(Avg('cost'))
-
-    return render(request, 'hello.html', {'name': 'Graham', 'count': count})
-
-
-# def say_hello(request):
-#     weapon = Weapon.objects.select_related('damage_type').all()
-#
-#     return render(request, 'hello.html', {'name': 'Graham', 'weapons': list(weapon)})
-
-
-# def say_hello(request):
-#     weapon = Weapon.objects.prefetch_related('properties').all()
-#
-#     return render(request, 'hello.html', {'name': 'Graham', 'weapons': list(weapon)})
