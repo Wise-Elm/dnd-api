fa440cb1536e566778c176022634ed9aee1ecf20
gsteeds <steeds.g@gmail.com>
2022-02-22 19:40:32 -0600

    added requirements.txt.

--- dnd5eapi/settings.py
+++ dnd5eapi/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = SK
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['drsteeds.pythonanywhere.com']
 
 
 # Application definition
